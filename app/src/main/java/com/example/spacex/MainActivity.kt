7525d1e964d66ee23a3a7a7daa495bc1024bb186
IhorB <igorbat15@hotmail.com>
2021-10-15 14:55:28 -0400

    Added Compose dependencies

--- app/src/main/java/com/example/spacex/MainActivity.kt
+++ app/src/main/java/com/example/spacex/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.spacex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard("Jimmy")
+        }
     }
+
+    @Preview
+    @Composable
+    fun MessageCard(name: String = "Test") {
+        Text(text = "Hello $name!")
+    }
+
 }
